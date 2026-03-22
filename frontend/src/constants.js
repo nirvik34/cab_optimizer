97159a58fee4f898dc444905d454595f7419b29e
nirvik34 <jarhnirvik@gmail.com>
2026-03-23 00:14:48 +0530

    added render url

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,7 @@
-export const API_URL = 'http://127.0.0.1:8000'
+const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+export const API_URL = isLocal
+  ? 'http://127.0.0.1:8000'
+  : 'https://cab-optimizer.onrender.com'
 export const MAP_CENTER = [12.8406, 80.1534]     // centered on VIT campus
 export const MAP_ZOOM = 15
 export const STEP_LABELS = ['PLACE USER', 'PLACE DRIVER', 'PLACE DESTINATION']
